8d4e74e9424bbeda441aba72c16ae56ff2278cef
Gokul.A <asgokul2004@gmail.com>
2026-09-25 15:14:28 +0530

    refactor(f06): remove duplicate legacy RenderFabric implementation

--- apps/web/factoryos/core/rendering/RenderFabric.ts
+++
@@ -1,6 +0,0 @@
-/**
- * @deprecated Compatibility shim.
- * Floor 06 Render Fabric is authoritative under ../fabric/RenderFabric.
- * New production code must import from core/fabric/RenderFabric.
- */
-export * from "../fabric/RenderFabric";
